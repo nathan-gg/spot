807c1909ae369a8e3bd05a244d0ea7b78cc84dc2
Tirsten <tkylelisondra2005@gmail.com>
2026-02-28 23:14:48 -0800

    Started initial Map API integrations (doesn't work yet)

--- App.js
+++ App.js
@@ -1,20 +1,6 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import MapScreen from "./src/screens/MapScreen";
+
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>hi, im spot and this is a new test of the in vscode git push</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  return <MapScreen />;
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
--- src/styles.js
+++ src/styles.js
@@ -1,1 +1,35 @@
-// hi
+import StyleSheet from react-native;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    padding: 10,
+  },
+  title: {
+    marginTop: 10,
+    padding: 5,
+    fontWeight: "bold",
+    fontSize: 20,
+  },
+  map: {
+    width: "100%",
+    height: "80%",
+  },
+  input: {
+    flex: 1,
+    borderWidth: 1,
+    borderColor: "black",
+    padding: 5,
+    borderRadius: 5,
+    margin: 10,
+  },
+  searchRow: {
+    flexDirection: "row",
+  },
+  buttonView: {
+    margin: 10,
+  },
+});
+
+export default styles;
